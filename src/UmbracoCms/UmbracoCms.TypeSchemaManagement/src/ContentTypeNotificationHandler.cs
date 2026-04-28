c43f873d180ba600f8c88dc54f8b67cd9fff62b1
Rasmus John Pedersen <mail@rjp.dk>
2026-04-28 15:12:17 +0200

    feat: enlist content type changes in scope

--- src/UmbracoCms/UmbracoCms.TypeSchemaManagement/src/ContentTypeNotificationHandler.cs
+++ src/UmbracoCms/UmbracoCms.TypeSchemaManagement/src/ContentTypeNotificationHandler.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Options;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Scoping;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Compose.Integrations.UmbracoCms.Core;
 
@@ -12,6 +13,7 @@ namespace Umbraco.Compose.Integrations.UmbracoCms.TypeSchemaManagement;
 internal class ContentTypeNotificationHandler(
     ChannelWriter<SchemaQueueItem> writer,
     IDataTypeService dataTypeService,
+    ICoreScopeProvider coreScopeProvider,
     ILogger<ContentTypeNotificationHandler> logger,
     IOptions<UmbracoComposeOptions> options) :
         INotificationAsyncHandler<ContentTypeSavedNotification>,
@@ -25,22 +27,51 @@ internal class ContentTypeNotificationHandler(
             return;
         }
 
-        foreach (IContentType contentType in notification.SavedEntities)
+        await ExecuteDeferredAsync(async () =>
         {
-            await writer.WriteAsync(new SchemaQueueItem(contentType.Alias), cancellationToken).ConfigureAwait(false);
-        }
+            foreach (IContentType contentType in notification.SavedEntities)
+            {
+                await writer.WriteAsync(new SchemaQueueItem(contentType.Alias), cancellationToken).ConfigureAwait(false);
+            }
+        })
+            .ConfigureAwait(false);
     }
 
     public async Task HandleAsync(DataTypeSavedNotification notification, CancellationToken cancellationToken)
     {
-        foreach (IDataType entity in notification.SavedEntities)
+        if (!options.Value.IsValid)
         {
-            PagedModel<RelationItemModel> relations = await dataTypeService.GetPagedRelationsAsync(entity.Key, 0, 1000)
-                .ConfigureAwait(false);
-            foreach (string contentTypeAlias in relations.Items.Select(x => x.ContentTypeAlias).OfType<string>())
-            {
-                await writer.WriteAsync(new SchemaQueueItem(contentTypeAlias), cancellationToken).ConfigureAwait(false);
-            }
+            logger.LogDebug("Skipping type schema update - Compose options are not valid.");
+            return;
         }
+
+        await ExecuteDeferredAsync(async () =>
+        {
+            foreach (IDataType entity in notification.SavedEntities)
+            {
+                PagedModel<RelationItemModel> relations = await dataTypeService.GetPagedRelationsAsync(entity.Key, 0, 1000)
+                    .ConfigureAwait(false);
+                foreach (string contentTypeAlias in relations.Items.Select(x => x.ContentTypeAlias).OfType<string>())
+                {
+                    await writer.WriteAsync(new SchemaQueueItem(contentTypeAlias), cancellationToken).ConfigureAwait(false);
+                }
+            }
+        })
+            .ConfigureAwait(false);
+    }
+
+    private ValueTask ExecuteDeferredAsync(Func<ValueTask> action)
+    {
+        DeferredActions? actions = DeferredActions.Get(coreScopeProvider);
+        if (actions is not null)
+        {
+            actions.Add(action);
+        }
+        else
+        {
+            return action();
+        }
+
+        return default;
     }
 }
